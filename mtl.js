b8b8b21ea0f3b2c24f9eeec17b6f10a68abb1c7b
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2013-07-16 18:03:49 -0400

    figured out my for loop problem

--- mtl.js
+++ mtl.js
@@ -1,32 +1,11 @@
 $(document).ready(function() {
 	
-	$.getJSON("data.json", function(data) {
-		
-		var span = "span4";
-		var first = "first";
-		var second = "second";
-		
-		// FOOD section
-		
-		var moment = "One goes there for: ";
-		var hood = "Located in: ";
-		var ulNotes = "<ul class =\"notes\">";
-		var ulContact = "<ul class =\"contact\">";
-		
-		
+	$.getJSON("data.json", function(data) {	
 		
 		// Québec ------------------------------------------------------------------------------------------------------------------------
 		
-		/*
-		 * name
-		 * hood
-		 * moment
-		 * contact > url[]
-		 * contact > address[]
-		 * contact > phone
-		 * notes[]
-		 */
-		
+		// data.cuisines.quebec.length
+		// data.cuisines.quebec[i].notes
 		
 		$("#eat > .cuisines").append("<div class=\"row first\">");
 		for (var i = 0; i < 3; i++) {
@@ -37,20 +16,37 @@ $(document).ready(function() {
 			$("#eat > .cuisines > .first > .block" + i).append("<p><a href=\"" + data.cuisines.quebec[i].url.link + "\">" + data.cuisines.quebec[i].url.text);
 			$("#eat > .cuisines > .first > .block" + i).append("<p><a href=\"" + data.cuisines.quebec[i].address.link + "\">" + data.cuisines.quebec[i].address.text);
 			$("#eat > .cuisines > .first > .block" + i).append("<p>" + data.cuisines.quebec[i].phone);
-			$("#eat > .cuisines > .first > .block" + i).append("<p>" + data.cuisines.quebec[i].notes);
+			for (var j = 0; j < data.cuisines.quebec[i].notes.length; j++) {
+				$("#eat > .cuisines > .first > .block" + i).append("<p>" + data.cuisines.quebec[i].notes[j]);
+			}
 		};
 		
 		$("#eat > .cuisines").append("<div class=\"row second\">");
 		for (var i = 3; i < 6; i++) {
-			//$("#eat > .cuisines > .row").empty()
-			
-			$("#eat > .cuisines > .second").append("<h5>" + data.cuisines.quebec[i].name);
-			$("#eat > .cuisines > .second").append("<p>" + data.cuisines.quebec[i].hood);
-			$("#eat > .cuisines > .second").append("<p>" + data.cuisines.quebec[i].moment);
-			$("#eat > .cuisines > .second").append("<p><a href=\"" + data.cuisines.quebec[i].adrLink + "\">" + data.cuisines.quebec[i].adrText);
-			$("#eat > .cuisines > .second").append("<p>" + data.cuisines.quebec[i].phone);
-			$("#eat > .cuisines > .second").append("<p>" + data.cuisines.quebec[i].notes);
+			$("#eat > .cuisines > .second").append("<div class=\"span4 block" + i + "\">");
+			$("#eat > .cuisines > .second > .block" + i).append("<h5>" + data.cuisines.quebec[i].name);
+			$("#eat > .cuisines > .second > .block" + i).append("<p>" + data.cuisines.quebec[i].hood);
+			$("#eat > .cuisines > .second > .block" + i).append("<p>" + data.cuisines.quebec[i].moment);
+			$("#eat > .cuisines > .second > .block" + i).append("<p><a href=\"" + data.cuisines.quebec[i].url.link + "\">" + data.cuisines.quebec[i].url.text);
+			$("#eat > .cuisines > .second > .block" + i).append("<p><a href=\"" + data.cuisines.quebec[i].address.link + "\">" + data.cuisines.quebec[i].address.text);
+			$("#eat > .cuisines > .second > .block" + i).append("<p>" + data.cuisines.quebec[i].phone);
+			for (var j = 0; j < data.cuisines.quebec[i].notes.length; j++) {
+				$("#eat > .cuisines > .second > .block" + i).append("<p>" + data.cuisines.quebec[i].notes[j]);
+			}
 		};
-
+		
+		/*
+		$("#eat > .cuisines").append("<div class=\"row second\">");
+		for (var i = 3; i < 6; i++) {
+			$("#eat > .cuisines > .second").append("<div class=\"span4 block" + i + "\">");
+			$("#eat > .cuisines > .second > .block" + i).append("<h5>" + data.cuisines.quebec[i].name);
+			$("#eat > .cuisines > .second > .block" + i).append("<p>" + data.cuisines.quebec[i].hood);
+			$("#eat > .cuisines > .second > .block" + i).append("<p>" + data.cuisines.quebec[i].moment);
+			$("#eat > .cuisines > .second > .block" + i).append("<p><a href=\"" + data.cuisines.quebec[i].url.link + "\">" + data.cuisines.quebec[i].url.text);
+			$("#eat > .cuisines > .second > .block" + i).append("<p><a href=\"" + data.cuisines.quebec[i].address.link + "\">" + data.cuisines.quebec[i].address.text);
+			$("#eat > .cuisines > .second > .block" + i).append("<p>" + data.cuisines.quebec[i].phone);
+			$("#eat > .cuisines > .second > .block" + i).append("<p>" + data.cuisines.quebec[i].notes);
+		};
+		*/
 	});
 });
