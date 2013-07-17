2538b3c3ecc71b0d058717fbcb0527d2dc609e25
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2013-07-17 10:28:09 -0400

    made a fuckin function

--- mtl.js
+++ mtl.js
@@ -7,44 +7,37 @@ $(document).ready(function() {
 		// section title
 		$("#eat").append("<div class=\"title row\"><div class=\"span12\"><h1>" + data.eat.title);
 		
-		$("#eat").append("<div class=\"quebec\">");
+		$("#eat").append("<div class=\"quebec\">").html();
 		$("#eat").append("<div class=\"france\">");
 		$("#eat").append("<div class=\"poutine\">");
 		
 		// Québec --------------------------------------
+		
+		function quebecData(rowNumber) {
+			$("#eat > .quebec > ." + rowNumber).append("<div class=\"span4 block" + i + "\">");
+			$("#eat > .quebec > ." + rowNumber + " > .block" + i).append("<h5>" + data.eat.cuisines.quebec.restaurants[i].name);
+			$("#eat > .quebec > ." + rowNumber + " > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].hood);
+			$("#eat > .quebec > ." + rowNumber + " > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].moment);
+			for (var j = 0; j < data.eat.cuisines.quebec.restaurants[i].notes.length; j++) {
+				$("#eat > .quebec > ." + rowNumber + " > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].notes[j]);
+			}
+			$("#eat > .quebec > ." + rowNumber + " > .block" + i).append("<ul>");
+			$("#eat > .quebec > ." + rowNumber + " > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.quebec.restaurants[i].url.link + "\">" + data.eat.cuisines.quebec.restaurants[i].url.text);
+			$("#eat > .quebec > ." + rowNumber + " > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.quebec.restaurants[i].address.link + "\">" + data.eat.cuisines.quebec.restaurants[i].address.text);
+			$("#eat > .quebec > ." + rowNumber + " > .block" + i + " > ul").append("<li>" + data.eat.cuisines.quebec.restaurants[i].phone);
+		};
 		
 		$("#eat > .quebec").append("<div class=\"section_title row\"><div class=\"span12\"><h3>" + data.eat.cuisines.quebec.region);
 		$("#eat > .quebec > .section_title > .span12").append("<h4>" + data.eat.cuisines.quebec.description);
 		
 		$("#eat > .quebec").append("<div class=\"row first\">");
 		for (var i = 0; i < 3; i++) {
-			$("#eat > .quebec > .first").append("<div class=\"span4 block" + i + "\">");
-			$("#eat > .quebec > .first > .block" + i).append("<h5>" + data.eat.cuisines.quebec.restaurants[i].name);
-			$("#eat > .quebec > .first > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].hood);
-			$("#eat > .quebec > .first > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].moment);
-			for (var j = 0; j < data.eat.cuisines.quebec.restaurants[i].notes.length; j++) {
-				$("#eat > .quebec > .first > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].notes[j]);
-			}
-			$("#eat > .quebec > .first > .block" + i).append("<ul>");
-			$("#eat > .quebec > .first > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.quebec.restaurants[i].url.link + "\">" + data.eat.cuisines.quebec.restaurants[i].url.text);
-			$("#eat > .quebec > .first > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.quebec.restaurants[i].address.link + "\">" + data.eat.cuisines.quebec.restaurants[i].address.text);
-			$("#eat > .quebec > .first > .block" + i + " > ul").append("<li>" + data.eat.cuisines.quebec.restaurants[i].phone);
+			quebecData("first");
 		};
 		
 		$("#eat > .quebec").append("<div class=\"row second\">");
 		for (var i = 3; i < 6; i++) {
-			$("#eat > .quebec > .second").append("<div class=\"span4 block" + i + "\">");
-			$("#eat > .quebec > .second > .block" + i).append("<h5>" + data.eat.cuisines.quebec.restaurants[i].name);
-			$("#eat > .quebec > .second > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].hood);
-			$("#eat > .quebec > .second > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].moment);
-			for (var j = 0; j < data.eat.cuisines.quebec.restaurants[i].notes.length; j++) {
-				$("#eat > .quebec > .second > .block" + i).append("<p>" + data.eat.cuisines.quebec.restaurants[i].notes[j]);
-			}
-			$("#eat > .quebec > .second > .block" + i).append("<ul>");
-			$("#eat > .quebec > .second > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.quebec.restaurants[i].url.link + "\">" + data.eat.cuisines.quebec.restaurants[i].url.text);
-			$("#eat > .quebec > .second > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.quebec.restaurants[i].address.link + "\">" + data.eat.cuisines.quebec.restaurants[i].address.text);
-			$("#eat > .quebec > .second > .block" + i + " > ul").append("<li>" + data.eat.cuisines.quebec.restaurants[i].phone);
-		
+			quebecData("second");
 		};
 		
 		// France --------------------------------------
@@ -85,6 +78,27 @@ $(document).ready(function() {
 			$("#eat > .poutine > .first > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.poutine.restaurants[i].url.link + "\">" + data.eat.cuisines.poutine.restaurants[i].url.text);
 			$("#eat > .poutine > .first > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.poutine.restaurants[i].address.link + "\">" + data.eat.cuisines.poutine.restaurants[i].address.text);
 			$("#eat > .poutine > .first > .block" + i + " > ul").append("<li>" + data.eat.cuisines.poutine.restaurants[i].phone);
+		};
+		
+		
+		// STAY ------------------------------------------------------------------------------------------------------------------------
+		
+		// section title
+		$("#stay").append("<div class=\"title row\"><div class=\"span12\"><h1>" + data.stay.title);
+		
+		$("#stay").append("<div class=\"plateau\">");
+		
+		// Poutine --------------------------------------
+		
+		//$("#stay > .plateau").append("<div class=\"section_title row\"><div class=\"span12\"><h3>" + data.stay.hoods.name);
+		
+		$("#stay > .plateau").append("<div class=\"row first\">");
+		for (var i = 0; i < data.stay.hoods.length; i++) {
+			$("#stay > .plateau > .first").append("<div class=\"span4 block" + i + "\">");
+			$("#stay > .plateau > .first > .block" + i).append("<h5>" + data.stay.hoods[i].name);
+			for (var j = 0; j < data.stay.hoods[i].about.length; j++) {
+				$("#stay > .plateau > .first > .block" + i).append("<p>" + data.stay.hoods[i].about[j]);
+			}
 		};
 		
 				
