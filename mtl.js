f6ff4532cab824232d04b6f1f9bb236513a881aa
papoirier <pa.poirier@gmail.com>
2013-11-01 10:59:16 -0400

    more content

--- mtl.js
+++ mtl.js
@@ -4,17 +4,16 @@ $(document).ready(function() {
 		
 		// EAT ------------------------------------------------------------------------------------------------------------------------
 		
-		// section title
-		$("#eat > .container").append("<div class=\"title row\"><div class=\"col-lg-12\"><h1>" + data.eat.title);
+		$("#eat > .container").append("<div class=\"title row\"><div class=\"col-md-12\"><h1>" + data.eat.title);
 		
 		$("#eat > .container").append("<div class=\"breakfast\">").html();
 		$("#eat > .container").append("<div class=\"dinner\">");
 		$("#eat > .container").append("<div class=\"any_hour\">");
 		
-		// BREAKFAST --------------------------------------
+		// breakfast --------------------------------------
 		
 		function breakfastData(rowNumber) {
-			$("#eat > .container > .breakfast > ." + rowNumber).append("<div class=\"col-lg-4 block" + i + "\">");
+			$("#eat > .container > .breakfast > ." + rowNumber).append("<div class=\"col-md-4 block" + i + "\">");
 			$("#eat > .container > .breakfast > ." + rowNumber + " > .block" + i).append("<h5>" + data.eat.cuisines.breakfast.restaurants[i].name);
 			$("#eat > .container > .breakfast > ." + rowNumber + " > .block" + i).append("<h6>" + data.eat.cuisines.breakfast.restaurants[i].hood);
 			$("#eat > .container > .breakfast > ." + rowNumber + " > .block" + i).append("<p class=\"moment\">" + data.eat.cuisines.breakfast.restaurants[i].moment);
@@ -27,8 +26,8 @@ $(document).ready(function() {
 			$("#eat > .container > .breakfast > ." + rowNumber + " > .block" + i + " > ul").append("<li>" + data.eat.cuisines.breakfast.restaurants[i].phone);
 		};
 		
-		$("#eat > .container > .breakfast").append("<div class=\"section_title row\"><div class=\"col-lg-12\"><h3>" + data.eat.cuisines.breakfast.region);
-		$("#eat > .container > .breakfast > .section_title > .col-lg-12").append("<h4>" + data.eat.cuisines.breakfast.description);
+		$("#eat > .container > .breakfast").append("<div class=\"section_title row\"><div class=\"col-md-12\"><h3>" + data.eat.cuisines.breakfast.region);
+		$("#eat > .container > .breakfast > .section_title > .col-md-12").append("<h4>" + data.eat.cuisines.breakfast.description);
 		
 		$("#eat > .container > .breakfast").append("<div class=\"row first\">");
 		for (var i = 0; i < 3; i++) {
@@ -40,14 +39,14 @@ $(document).ready(function() {
 			breakfastData("second");
 		};
 		
-		// DINNER --------------------------------------
+		// dinner --------------------------------------
 		
-		$("#eat > .container > .dinner").append("<div class=\"section_title row\"><div class=\"col-lg-12\"><h3>" + data.eat.cuisines.dinner.region);
-		$("#eat > .container > .dinner > .section_title > .col-lg-12").append("<h4>" + data.eat.cuisines.dinner.description);
+		$("#eat > .container > .dinner").append("<div class=\"section_title row\"><div class=\"col-md-12\"><h3>" + data.eat.cuisines.dinner.region);
+		$("#eat > .container > .dinner > .section_title > .col-md-12").append("<h4>" + data.eat.cuisines.dinner.description);
 		
 		$("#eat > .container > .dinner").append("<div class=\"row first\">");
 		for (var i = 0; i < data.eat.cuisines.dinner.restaurants.length; i++) {
-			$("#eat > .container > .dinner > .first").append("<div class=\"col-lg-4 block" + i + "\">");
+			$("#eat > .container > .dinner > .first").append("<div class=\"col-md-4 block" + i + "\">");
 			$("#eat > .container > .dinner > .first > .block" + i).append("<h5>" + data.eat.cuisines.dinner.restaurants[i].name);
 			$("#eat > .container > .dinner > .first > .block" + i).append("<h6>" + data.eat.cuisines.dinner.restaurants[i].hood);
 			$("#eat > .container > .dinner > .first > .block" + i).append("<p class=\"moment\">" + data.eat.cuisines.dinner.restaurants[i].moment);
@@ -60,38 +59,62 @@ $(document).ready(function() {
 			$("#eat > .container > .dinner > .first > .block" + i + " > ul").append("<li>" + data.eat.cuisines.dinner.restaurants[i].phone);
 		};
 		
-		// any_hour --------------------------------------
+		// any hour --------------------------------------
 		
-		$("#eat > .container > .any_hour").append("<div class=\"section_title row\"><div class=\"col-lg-12\"><h3>" + data.eat.cuisines.any_hour.region);
-		$("#eat > .container > .any_hour > .section_title > .col-lg-12").append("<h4>" + data.eat.cuisines.any_hour.description);
+		function anyHourData(rowNumber) {
+			$("#eat > .container > .any_hour > ." + rowNumber).append("<div class=\"col-md-4 block" + i + "\">");
+			$("#eat > .container > .any_hour > ." + rowNumber + " > .block" + i).append("<h5>" + data.eat.cuisines.any_hour.restaurants[i].name);
+			$("#eat > .container > .any_hour > ." + rowNumber + " > .block" + i).append("<h6>" + data.eat.cuisines.any_hour.restaurants[i].hood);
+			$("#eat > .container > .any_hour > ." + rowNumber + " > .block" + i).append("<p class=\"moment\">" + data.eat.cuisines.any_hour.restaurants[i].moment);
+			for (var j = 0; j < data.eat.cuisines.any_hour.restaurants[i].notes.length; j++) {
+				$("#eat > .container > .any_hour > ." + rowNumber + " > .block" + i).append("<p>" + data.eat.cuisines.any_hour.restaurants[i].notes[j]);
+			}
+			$("#eat > .container > .any_hour > ." + rowNumber + " > .block" + i).append("<ul>");
+			$("#eat > .container > .any_hour > ." + rowNumber + " > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.any_hour.restaurants[i].url.link + "\">" + data.eat.cuisines.any_hour.restaurants[i].url.text);
+			$("#eat > .container > .any_hour > ." + rowNumber + " > .block" + i + " > ul").append("<li>" + data.eat.cuisines.any_hour.restaurants[i].address.text);
+			$("#eat > .container > .any_hour > ." + rowNumber + " > .block" + i + " > ul").append("<li>" + data.eat.cuisines.any_hour.restaurants[i].phone);
+		};
+		
+		$("#eat > .container > .any_hour").append("<div class=\"section_title row\"><div class=\"col-md-12\"><h3>" + data.eat.cuisines.any_hour.region);
+		$("#eat > .container > .any_hour > .section_title > .col-md-12").append("<h4>" + data.eat.cuisines.any_hour.description);
 		
 		$("#eat > .container > .any_hour").append("<div class=\"row first\">");
-		for (var i = 0; i < data.eat.cuisines.any_hour.restaurants.length; i++) {
-			$("#eat > .container > .any_hour > .first").append("<div class=\"col-lg-4 block" + i + "\">");
-			$("#eat > .container > .any_hour > .first > .block" + i).append("<h5>" + data.eat.cuisines.any_hour.restaurants[i].name);
-			$("#eat > .container > .any_hour > .first > .block" + i).append("<h6>" + data.eat.cuisines.any_hour.restaurants[i].hood);
-			$("#eat > .container > .any_hour > .first > .block" + i).append("<p class=\"moment\">" + data.eat.cuisines.any_hour.restaurants[i].moment);
-			for (var j = 0; j < data.eat.cuisines.any_hour.restaurants[i].notes.length; j++) {
-				$("#eat > .container > .any_hour > .first > .block" + i).append("<p>" + data.eat.cuisines.any_hour.restaurants[i].notes[j]);
-			}
-			$("#eat > .container > .any_hour > .first > .block" + i).append("<ul>");
-			$("#eat > .container > .any_hour > .first > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.any_hour.restaurants[i].url.link + "\">" + data.eat.cuisines.any_hour.restaurants[i].url.text);
-			$("#eat > .container > .any_hour > .first > .block" + i + " > ul").append("<li>" + data.eat.cuisines.any_hour.restaurants[i].address.text);
-			$("#eat > .container > .any_hour > .first > .block" + i + " > ul").append("<li>" + data.eat.cuisines.any_hour.restaurants[i].phone);
+		for (var i = 0; i < 3; i++) {
+			anyHourData("first");
 		};
+		
+		$("#eat > .container > .any_hour").append("<div class=\"row second\">");
+		for (var i = 3; i < 4; i++) {
+			anyHourData("second");
+		};
+		
+
+		// $("#eat > .container > .any_hour").append("<div class=\"row first\">");
+		// for (var i = 0; i < data.eat.cuisines.any_hour.restaurants.length; i++) {
+		// 	$("#eat > .container > .any_hour > .first").append("<div class=\"col-md-4 block" + i + "\">");
+		// 	$("#eat > .container > .any_hour > .first > .block" + i).append("<h5>" + data.eat.cuisines.any_hour.restaurants[i].name);
+		// 	$("#eat > .container > .any_hour > .first > .block" + i).append("<h6>" + data.eat.cuisines.any_hour.restaurants[i].hood);
+		// 	$("#eat > .container > .any_hour > .first > .block" + i).append("<p class=\"moment\">" + data.eat.cuisines.any_hour.restaurants[i].moment);
+		// 	for (var j = 0; j < data.eat.cuisines.any_hour.restaurants[i].notes.length; j++) {
+		// 		$("#eat > .container > .any_hour > .first > .block" + i).append("<p>" + data.eat.cuisines.any_hour.restaurants[i].notes[j]);
+		// 	}
+		// 	$("#eat > .container > .any_hour > .first > .block" + i).append("<ul>");
+		// 	$("#eat > .container > .any_hour > .first > .block" + i + " > ul").append("<li><a href=\"" + data.eat.cuisines.any_hour.restaurants[i].url.link + "\">" + data.eat.cuisines.any_hour.restaurants[i].url.text);
+		// 	$("#eat > .container > .any_hour > .first > .block" + i + " > ul").append("<li>" + data.eat.cuisines.any_hour.restaurants[i].address.text);
+		// 	$("#eat > .container > .any_hour > .first > .block" + i + " > ul").append("<li>" + data.eat.cuisines.any_hour.restaurants[i].phone);
+		// };
 		
 		
 		// STAY ------------------------------------------------------------------------------------------------------------------------
 		
-		// section title
-		$("#stay > .container").append("<div class=\"title row\"><div class=\"col-lg-12\"><h1>" + data.stay.title);
-		$("#stay > .container").append("<div class=\"title row\"><div class=\"col-lg-12\"><h4>" + data.stay.description);
+		$("#stay > .container").append("<div class=\"title row\"><div class=\"col-md-12\"><h1>" + data.stay.title);
+		$("#stay > .container").append("<div class=\"title row\"><div class=\"col-md-12\"><h4>" + data.stay.description);
 		
 		// neighborhoods
 		$("#stay > .container").append("<div class=\"neighbourhoods\">");
 		
 		function hoodData(rowNumber) {
-			$("#stay > .container > .neighbourhoods > ." + rowNumber).append("<div class=\"col-lg-4 block" + i + "\">");
+			$("#stay > .container > .neighbourhoods > ." + rowNumber).append("<div class=\"col-md-4 block" + i + "\">");
 			$("#stay > .container > .neighbourhoods > ." + rowNumber + " > .block" + i).append("<h5>" + data.stay.hoods[i].name);
 			$("#stay > .container > .neighbourhoods > ." + rowNumber + " > .block" + i).append("<img src=\"img/" + data.stay.hoods[i].mapImage + "\" class=\"img-responsive\">");
 			
@@ -119,10 +142,10 @@ $(document).ready(function() {
 
 		// hotels
 		$("#stay > .container").append("<div class=\"hotels\">");
-		$("#stay > .container > .hotels").append("<div class=\"title row\"><div class=\"col-lg-12\"><h3>" + data.stay.hotel_description);
+		$("#stay > .container > .hotels").append("<div class=\"title row\"><div class=\"col-md-12\"><h3>" + data.stay.hotel_description);
 
 		function hotelData(rowNumber) {
-			$("#stay > .container > .hotels > ." + rowNumber).append("<div class=\"col-lg-4 block" + i + "\">");
+			$("#stay > .container > .hotels > ." + rowNumber).append("<div class=\"col-md-4 block" + i + "\">");
 			$("#stay > .container > .hotels > ." + rowNumber + " > .block" + i).append("<h5>" + data.stay.hotels[i].name);
 			$("#stay > .container > .hotels > ." + rowNumber + " > .block" + i).append("<ul>");
 			$("#stay > .container > .hotels > ." + rowNumber + " > .block" + i + " > ul").append("<li><a href=\"" + data.stay.hotels[i].url.link + "\">" + data.stay.hotels[i].url.text);
@@ -137,14 +160,13 @@ $(document).ready(function() {
 		
 		// TRANSPORT --------------------------------------------------------------------------------------------------------------------
 		
-		// section title
-		$("#transport > .container").append("<div class=\"title row\"><div class=\"col-lg-12\"><h1>" + data.transport.title);
-		$("#transport > .container").append("<div class=\"title row\"><div class=\"col-lg-12\"><h4>" + data.transport.description);
+		$("#transport > .container").append("<div class=\"title row\"><div class=\"col-md-12\"><h1>" + data.transport.title);
+		$("#transport > .container").append("<div class=\"title row\"><div class=\"col-md-12\"><h4>" + data.transport.description);
 		
 		$("#transport > .container").append("<div class=\"move\">");
 
 		function moveData(rowNumber) {
-			$("#transport > .container > .move > ." + rowNumber).append("<div class=\"col-lg-4 block" + i + "\">");
+			$("#transport > .container > .move > ." + rowNumber).append("<div class=\"col-md-4 block" + i + "\">");
 			$("#transport > .container > .move > ." + rowNumber + " > .block" + i).append("<h5>" + data.transport.service[i].name);
 			$("#transport > .container > .move > ." + rowNumber + " > .block" + i).append("<p>" + data.transport.service[i].type);
 			for (var j = 0; j < data.transport.service[i].notes.length; j++) {
@@ -166,16 +188,15 @@ $(document).ready(function() {
 		
 		// DRINK ------------------------------------------------------------------------------------------------------------------------
 		
-		// section title
-		$("#drink > .container").append("<div class=\"title row\"><div class=\"col-lg-12\"><h1>" + data.drink.title);
-		$("#drink > .container").append("<div class=\"title row\"><div class=\"col-lg-12\"><h4>" + data.drink.description);
+		$("#drink > .container").append("<div class=\"title row\"><div class=\"col-md-12\"><h1>" + data.drink.title);
+		$("#drink > .container").append("<div class=\"title row\"><div class=\"col-md-12\"><h4>" + data.drink.description);
 		
 		$("#drink > .container").append("<div class=\"bars\">");
 
 		function drinkData(rowNumber) {
-			$("#drink > .container > .bars > ." + rowNumber).append("<div class=\"col-lg-4 block" + i + "\">");
+			$("#drink > .container > .bars > ." + rowNumber).append("<div class=\"col-md-4 block" + i + "\">");
 			$("#drink > .container > .bars > ." + rowNumber + " > .block" + i).append("<h5>" + data.drink.bars[i].name);
-			$("#drink > .container > .bars > ." + rowNumber + " > .block" + i).append("<p>" + data.drink.bars[i].hood);
+			$("#drink > .container > .bars > ." + rowNumber + " > .block" + i).append("<h6>" + data.drink.bars[i].hood);
 			$("#drink > .container > .bars > ." + rowNumber + " > .block" + i).append("<p class=\"moment\">" + data.drink.bars[i].moment);
 			for (var j = 0; j < data.drink.bars[i].notes.length; j++) {
 				$("#drink > .container > .bars > ." + rowNumber + " > .block" + i).append("<p>" + data.drink.bars[i].notes[j]);
@@ -196,13 +217,18 @@ $(document).ready(function() {
 			drinkData("second");
 		};
 
+		$("#drink > .container > .bars").append("<div class=\"row third\">");
+		for (var i = 6; i < 8; i++) {
+			drinkData("third");
+		};
+
 
 		
 		// SIGHTS ------------------------------------------------------------------------------------------------------------------------
 		
 		// section title
-		$("#sights").append("<div class=\"title row\"><div class=\"col-lg-12\"><h1>" + data.sights.title);
-		$("#sights").append("<div class=\"title row\"><div class=\"col-lg-12\"><h4>" + data.sights.description);
+		$("#sights").append("<div class=\"title row\"><div class=\"col-md-12\"><h1>" + data.sights.title);
+		$("#sights").append("<div class=\"title row\"><div class=\"col-md-12\"><h4>" + data.sights.description);
 		
 		$("#sights").append("<div class=\"places\">");
 		
